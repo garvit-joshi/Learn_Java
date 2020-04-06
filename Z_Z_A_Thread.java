c75d59e04267ed03642a639b091a47b2956b576c
garvit-joshi <garvitjoshi9@gmail.com>
2020-04-06 15:09:13 +0530

    Renamed A File

--- Z_Z_A_Thread.java
+++ Z_Z_A_Thread.java
@@ -3,7 +3,7 @@ class Loader implements Runnable {
         System.out.println("Hello");
     }
 }
-class ZZA_Thread {
+class Z_Z_A_Thread {
     public static void main(String[ ] args) {
         Thread t = new Thread(new Loader());
         t.start();
